b28b9bea1779029ba50d56b6eb07add1a91c26a5
Dawid <dawidpiecha88@gmail.com>
2023-10-07 13:17:45 +0200

    added loadMore funcionality to get more than 40 pictures and add them to previous results

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ const itemsPerPage = 40;
 let searchInputTerm = '';
 let page = 1;
 let data;
+
 const searchInputField = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
 const galleryContainer = document.querySelector('.gallery');
@@ -20,11 +21,7 @@ const loadMoreButton = document.querySelector('.load-more');
 const resetButton = document.querySelector('.reset-button');
 
 resetButton.style.display = 'none';
-
-const clearGallery = () => {
-  galleryContainer.innerHTML = '';
-};
-clearGallery();
+loadMoreButton.style.display = 'none';
 
 const fetchGallery = async () => {
   const url = `?key=${apiKey}&q=${searchInputTerm}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${itemsPerPage}`;
@@ -36,9 +33,48 @@ const fetchGallery = async () => {
   }
 };
 
+const clearGallery = () => {
+  galleryContainer.innerHTML = '';
+};
+clearGallery();
+
+const displayGallery = hits => {
+  const galleryElements = hits
+    .map(hit => {
+      const {
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = hit;
+      return `<div class="photo-card"> <a href="${largeImageURL}">
+    <img class="picture" src="${webformatURL}" alt="${tags}" loading="lazy" title="${tags}"/></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes </b></br>${likes}
+      </p>
+      <p class="info-item">
+        <b>Views </b></br>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments </b></br>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads </b></br>${downloads}
+      </p>
+    </div>
+  </div>`;
+    })
+    .join('');
+  galleryContainer.innerHTML = galleryElements;
+};
+
 searchButton.addEventListener('click', async event => {
   event.preventDefault();
-  clearGallery();
+
   searchInputTerm = searchInputField.value.trim();
 
   if (!searchInputTerm) {
@@ -55,43 +91,33 @@ searchButton.addEventListener('click', async event => {
   const hits = data.hits;
 
   if (hits.length === 0) {
-    Notiflix.Notify.warning(errorMessage2);
+    Notiflix.Notify.failure(errorMessage2);
     return;
   }
-  const galleryElements = hits
-    .map(hit => {
-      const {
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      } = hit;
-      return `<div class="photo-card"> <a href="${largeImageURL}">
-      <img class="picture" src="${webformatURL}" alt="${tags}" loading="lazy" title="${tags}"/></a>
-      <div class="info">
-        <p class="info-item">
-          <b>Likes </b></br>${likes}
-        </p>
-        <p class="info-item">
-          <b>Views </b></br>${views}
-        </p>
-        <p class="info-item">
-          <b>Comments </b></br>${comments}
-        </p>
-        <p class="info-item">
-          <b>Downloads </b></br>${downloads}
-        </p>
-      </div>
-    </div>`;
-    })
-    .join('');
-
-  galleryContainer.innerHTML = galleryElements;
+  displayGallery(hits);
 
   resetButton.style.display = 'inline';
+  loadMoreButton.style.display = 'flex';
 
   const lightbox = new SimpleLightbox('.gallery a');
+});
+
+loadMoreButton.addEventListener('click', async () => {
+  page++;
+
+  const newData = await fetchGallery();
+
+  if (!newData) {
+    Notiflix.Notify.failure('Failed to load more images.');
+    return;
+  }
+
+  const newHits = newData.hits;
+
+  if (newHits.length === 0) {
+    Notiflix.Notify.info('No more images available.');
+    return;
+  }
+  data.hits = [...data.hits, ...newHits];
+  displayGallery(data.hits);
 });
